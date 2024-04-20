c31c4b413af41574ebff607ec2a78a678e037a6d
Kauri <kauri.off@gmail.com>
2024-04-20 17:58:26 +0300

    scary update

--- src/checker/updater.rs
+++ src/checker/updater.rs
@@ -10,22 +10,24 @@ pub async fn update() -> Result<Vec<SocketAddr>, tokio_rusqlite::Error> {
         .await
         .unwrap();
 
-    let res = conn.call(|conn| {
-        let mut result = Vec::new();
-        let mut stmt = conn
-            .prepare("SELECT ip, port FROM 'mc_server' UNION SELECT ip, port FROM 'open_port'")?;
-        let server_iter = stmt.query_map([], |row| {
-            let ip: String = row.get(0)?;
-            Ok(SocketAddr::new(ip.parse().unwrap(), row.get(1)?))
-        })?;
-        for server in server_iter {
-            if let Ok(server) = server {
-                result.push(server);
+    let res = conn
+        .call(|conn| {
+            let mut result = Vec::new();
+            let mut stmt = conn.prepare(
+                "SELECT ip, port FROM 'mc_server' UNION SELECT ip, port FROM 'open_port'",
+            )?;
+            let server_iter = stmt.query_map([], |row| {
+                let ip: String = row.get(0)?;
+                Ok(SocketAddr::new(ip.parse().unwrap(), row.get(1)?))
+            })?;
+            for server in server_iter {
+                if let Ok(server) = server {
+                    result.push(server);
+                }
             }
-        }
-        Ok(result)
-    })
-    .await;
+            Ok(result)
+        })
+        .await;
     conn.close().await.unwrap();
 
     fs::remove_file("./db/database.db").unwrap();
--- src/main.rs
+++ src/main.rs
@@ -13,42 +13,35 @@ mod checker;
 mod packet;
 mod scanner;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("MC Search: Starting");
+    let threads = 100;
 
-    let threads = 150;
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(threads + 3)
-        .enable_all()
-        .build()
-        .unwrap();
+    let (tx, rx) = mpsc::channel(5);
+    let tx = Arc::new(Mutex::new(tx));
 
-    runtime.block_on(async {
-        let (tx, rx) = mpsc::channel(5);
-        let tx = Arc::new(Mutex::new(tx));
+    let servers = update().await;
+    let _handler = task::spawn(port_handler(rx));
 
-        let servers = update().await;
-        let _handler = task::spawn(port_handler(rx));
+    if let Ok(servers) = servers {
+        for server in servers {
+            tx.lock().await.send(server).await.unwrap();
+        }
+    }
 
-        if let Ok(servers) = servers {
-            for server in servers {
-                tx.lock().await.send(server).await.unwrap();
-            }
+    loop {
+        let mut task_list = vec![];
+        for _ in 0..threads {
+            let mutex_tx = Arc::clone(&tx);
+            let task = task::spawn(check_random_ip(mutex_tx));
+            task_list.push(task);
         }
 
-        loop {
-            let mut task_list = vec![];
-            for _ in 0..threads {
-                let mutex_tx = Arc::clone(&tx);
-                let task = task::spawn(check_random_ip(mutex_tx));
-                task_list.push(task);
-            }
-
-            for task in task_list {
-                task.await.expect("Failed to await task");
-            }
+        for task in task_list {
+            task.await.expect("Failed to await task");
         }
-    });
+    }
 }
 
 #[cfg(test)]
